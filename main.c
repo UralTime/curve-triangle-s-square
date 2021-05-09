a798949dd70db8ea35c43262c37bbdfdbb751435
UralTime <79019835+UralTime@users.noreply.github.com>
2021-05-09 15:17:02 +0300

    mega step1
    Сначала поудобнее (int1-int2-int3 вместо int12+int13) площадь треугольника вычислена для функций из задания, остальные тесты просто для тестирования функцией test

--- main.c
+++ main.c
@@ -3,33 +3,30 @@
 #define eps 0.001
 
 // example from my task
-double ff1 (double x) { return (0.35 * x * x - 0.95 * x + 2.7);}
-double ff3 (double x) { return (3 * x + 1);}
-double ff2 (double x) { return (1 / (x + 2));}
-double ff1diff (double x) { return (0.7 * x - 0.95);}
-double ff3diff (double x) { return 3;}
-double ff2diff (double x) { return (-1 / ((x + 2) * (x + 2)));}
-double ff12 (double x) { return fabs(ff1(x) - ff2(x));}
-double ff13 (double x) { return fabs(ff1(x) - ff3(x));}
+double f1 (double x) { return (0.35 * x * x - 0.95 * x + 2.7);}
+double f2 (double x) { return (1 / (x + 2));}
+double f3 (double x) { return (3 * x + 1);}
+double df1 (double x) { return (0.7 * x - 0.95);}
+double df2 (double x) { return (-1 / ((x + 2) * (x + 2)));}
+double df3 (double x) { return 3;}
 
-// test examples
-double f1 (double x) { return exp(2 * x - 15);}
-double f2 (double x) { return 5 * sin(x);}
-double f3 (double x) { return 2 * atan(x);}
-double f1diff (double x) { return 2 * f1(x);}
-double f2diff (double x) { return 5 * cos(x);}
-double f3diff (double x) { return 2 / (1 + x * x);}
-double f12 (double x) { return fabs(f1(x) - f2(x));}
-double f13 (double x) { return fabs(f1(x) - f3(x));}
+// first test example
+double g1 (double x) { return exp(2 * x - 15);}
+double g2 (double x) { return 5 * sin(x);}
+double g3 (double x) { return 2 * atan(x);}
+double dg1 (double x) { return 2 * exp(2 * x - 15);}
+double dg2 (double x) { return 5 * cos(x);}
+double dg3 (double x) { return 2 / (1 + x * x);}
 
-//final test example
+// second test example
 double h1 (double x) { return 10 * log(x);}
 double h2 (double x) { return x * x * x;}
 double h3 (double x) { return x * x * x * x - x - 20;}
-double h1diff (double x) { return 10 / x;}
-double h2diff (double x) { return 3 * x * x;}
-double h3diff (double x) { return 4 * x * x * x - 1;}
+double dh1 (double x) { return 10 / x;}
+double dh2 (double x) { return 3 * x * x;}
+double dh3 (double x) { return 4 * x * x * x - 1;}
 
+int PRINT_ITERS = 0;
 double root (double (*f)(double), double (*g)(double), double a, double b, double eps1,
              double (*fdiff)(double), double (*gdiff)(double)) {
     double mid = (a + b) / 2, x = b;
@@ -42,12 +39,13 @@ double root (double (*f)(double), double (*g)(double), double a, double b, doubl
         x = a;
         eps1 = -eps1; // compare signs of x and x+eps, approximation from the left instead of right
     }
-    //int cnt = 0;
+    int iters = 0;
     do {
         x -= (f(x) - g(x)) / (fdiff(x) - gdiff(x));
-        //cnt++;
+        iters++;
     } while (((f(x) - g(x)) * (f(x - eps1) - g(x - eps1)) > 0) || (fabs(f(x - eps1) - g(x - eps1)) > 1));
-    //printf("we find root by %d steps ", cnt);
+    if (PRINT_ITERS)
+        printf("root is found by %d iterations\n", iters);
     return x;
 }
 
@@ -70,51 +68,51 @@ double integral (double (*f)(double), double a, double b, double eps2) {
     return sum2 * delta;
 }
 
-void test (const char* s1, const char* s2, const char* s3, double a, double b, double expect, double res, double eps3) {
-    printf("Calculating %s for %s - %s in [%f; %f] with precision %f, expected result = %f, calculated result = %f\n", s3, s1, s2, a, b, eps3, expect, res);
-    if (fabs(res - expect) < eps3)
-        printf("calculating answer differs from expected answer less than %f, good!\n", eps3);
+void test (const char *mode, const char* func, double left, double right, double expect, double res, double eps1) {
+    printf("%s of %s in [%f; %f], expected %f, calculated %f\n", mode, func, left, right, expect, res);
+    if (fabs(res - expect) < eps1)
+        printf("GOOD! Calculating answer differs from expected less than %f\n\n", eps1);
     else
-        printf("calculating answer differs from expected answer more than %f, bad!\n", eps3);
+        printf("BAD! Calculating answer differs from expected more than %f\n\n", eps1);
 }
 
 int main () {
-    double epss = eps / 10;
-    double root1 = root(f1, f2, 6, 8.1, epss, f1diff, f2diff),
-        root2 = root(f2, f3, 6, 8.1, epss, f2diff, f3diff),
-        root3 = root(f3, f1, 6, 8.1, epss, f3diff, f1diff),
-        int1 = integral(f12, root1 , root2, epss),
-        int2 = integral(f13, root2, root3, epss);
-    test("5sinx", "exp(2x-15)", "root", 6, 8.1, 6.301380, root1, epss);
-    test("2arctgx", "5sinx", "root", 6, 8.1, 6.890506, root2, epss);
-    test("2arctgx", "exp(2x-15)", "root", 6, 8.1, 8.031292, root3, epss);
-    test("5sinx", "exp(2x-15)", "integral", root1, root2, 0.790995, int1, epss);
-    test("2arctgx", "exp(2x-15)", "integral", root2, root3, 1.980160, int2, epss);
-    printf("Calculating curve triangle's square, expected result = 2.771155, calculating result = %f\n", int1 + int2);
-    if (fabs(int1 + int2 - 2.771155) < eps) printf("calculating answer differs from expected answer less then eps = 0.001, good!\n\n");
-    else printf("calculating answer differs from expected answer more then eps = 0.001, bad!\n\n");
-    printf("new test!\n");
-    root1 = root(ff1, ff2, -1.99, -1.3, epss, ff1diff, ff2diff),
-    root2 = root(ff2, ff3, -1.99, 1, epss, ff2diff, ff3diff),
-    root3 = root(ff3, ff1, -1.99, 1, epss, ff3diff, ff1diff),
-    int1 = integral(ff12, root1, root2, epss),
-    int2 = integral(ff13, root2, root3, epss);
-    test("0.35x^2 - 0.95x + 2.7", "1 / (x+2)", "root", -1.99, -1.3, -1.821140, root1, epss);
-    test("3x + 1", "1 / (x+2)", "root", -1.99, 1, -0.152872, root2, epss);
-    test("0.35x^2 - 0.95x + 2.7", "(3x + 1)", "root", -1.99, 1, 0.448178, root3, epss);
-    test("0.35x^2 - 0.95x + 2.7", "1 / (x+2)", "integral", root1, root2, 4.438040, int1, epss);
-    test("0.35x^2 - 0.95x + 2.7", "(3x + 1)", "integral", root2, root3, 0.682157, int2, epss);
-    printf("Calculating curve triangle's square, expected result = 5.120197, calculating result = %f\n", int1 + int2);
-    if (fabs(int1 + int2 - 5.120197) < eps) printf("calculating answer differs from expected answer less then eps = 0.001, good!\n\n");
-    else printf("calculating answer differs from expected answer more then eps = 0.001, bad!\n\n");
-    printf("new test, not triangle's square, just roots and integrals!\n");
-    root1 = root(h1, h2, 1, 1.5, epss, h1diff, h2diff),
-    root2 = root(h2, h3, 2, 3, epss, h2diff, h3diff),
-    root3 = root(h3, h1, 2, 3, epss, h3diff, h1diff),
-    test("10lnx", "x^3", "root", 1, 1.5, 1.177194, root1, epss);
-    test("x^3", "(x^4 - x - 20)", "root", 2, 3, 2.477724, root2, epss);
-    test("10lnx", "(x^4 - x - 20)", "root", 2, 3, 2.35844, root3, epss);
-    test("lnx", "0", "integral", 1, 2, 3.862943, integral(h1, 1, 2, epss), epss);
-    test("x^4 - x - 20", "0", "integral", 3, 5, 528.40000, integral(h3, 3, 5, epss), epss);
+    const char *mode_root = "root", *mode_int = "integral";
+    double epss = eps / 10,
+    root1 = root(f1, f2, -1.99, -1.3, epss, df1, df2), root2 = root(f2, f3, -1.99, 1, epss, df2, df3),
+    root3 = root(f3, f1, -1.99, 1, epss, df3, df1),
+    int1 = integral(f1, root1, root3, epss), int2 = integral(f2, root1, root2, epss),
+    int3 = integral(f3, root2, root3, epss), ans = fabs(int1 - int2 - int3);
+    test(mode_root, "0.35x^2 - 0.95x + 2.7 - 1 / (x+2)", -1.99, -1.3, -1.821140, root1, epss);
+    test(mode_root, "3x + 1 - 1 / (x+2)", -1.99, 1, -0.152872, root2, epss);
+    test(mode_root, "0.35x^2 - 0.95x + 2.7 - (3x + 1)", -1.99, 1, 0.448178, root3, epss);
+    test(mode_int, "0.35x^2 - 0.95x + 2.7", root1, root3, 8.32227, int1, epss);
+    test(mode_int, "1 / (x+2)", root1, root2, 2.33478, int2, epss);
+    test(mode_int, "(3x + 1)", root2, root3, 0.867291, int3, epss);
+    printf("Curve triangle's square, expected 5.120197, calculating %f\n", ans);
+    if (fabs(ans - 5.120197) < eps) printf("GOOD! Calculating answer differs from expected less than 0.001\n\n");
+    else printf("BAD! Calculating answer differs from expected more than 0.001\n\n");
+
+    printf("new functions!\n\n");
+    root1 = root(g1, g2, 6, 8.1, epss, dg1, dg2), root2 = root(g2, g3, 6, 8.1, epss, dg2, dg3),
+    root3 = root(g3, g1, 6, 8.1, epss, dg3, dg1),
+    int1 = integral(g1, root1 , root3, epss), int2 = integral(g2, root1, root2, epss),
+    int3 = integral(g3, root2, root3, epss);
+    test(mode_root, "5sinx - e^(2x-15)", 6, 8.1, 6.301380, root1, epss);
+    test(mode_root, "2arctgx - 5sinx", 6, 8.1, 6.890506, root2, epss);
+    test(mode_root, "2arctgx - e^(2x-15)", 6, 8.1, 8.031292, root3, epss);
+    test(mode_int, "e^(2x - 15)", root1, root3, 1.40144, int1, epss);
+    test(mode_int, "5sinx", root1, root2, 0.893273, int2, epss);
+    test(mode_int, "2arctgx", root2, root3, 3.27932, int3, epss);
+
+    printf("new functions!\n");
+    root1 = root(h1, h2, 1, 1.5, epss, dh1, dh2),
+    root2 = root(h2, h3, 2, 3, epss, dh2, dh3),
+    root3 = root(h3, h1, 2, 3, epss, dh3, dh1),
+    test(mode_root,"10lnx - x^3", 1, 1.5, 1.177194, root1, epss);
+    test(mode_root, "x^4 - x - 20 - x^3", 2, 3, 2.477724, root2, epss);
+    test(mode_root, "x^4 - x - 20 - 10lnx", 2, 3, 2.35844, root3, epss);
+    test(mode_int,"lnx", 1, 2, 3.862943, integral(h1, 1, 2, epss), epss);
+    test(mode_int,"x^4 - x - 20", 3, 5, 528.40000, integral(h3, 3, 5, epss), epss);
     return 0;
 }
