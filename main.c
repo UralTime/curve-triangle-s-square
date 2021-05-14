9b7c095b511478c89f5cb50956662871c9c1c1b4
UralTime <79019835+UralTime@users.noreply.github.com>
2021-05-14 21:55:08 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ extern double df1 (double x);
 extern double df2 (double x);
 extern double df3 (double x);
 
-enum {DEFAULT, TEST_ROOT, TEST_INTEGRAL};
+enum {DEFAULT, TEST_ROOT, TEST_INTEGRAL, TEST_DEFAULT};
 
 int PRINT_POINTS = 0,
         PRINT_ITERS = 0,
@@ -69,12 +69,14 @@ Functions:\n\
 \n\
 General options:\n\
     -help                show help message\n\
-    -points              display intersection points\n\
-    -iters               display number of iterations in root()\n\
+    -verbose (-v)        show detailed information about mission of the program\n\
+    -points (-p)         display intersection points\n\
+    -iters  (-i)         display number of iterations in root()\n\
 \n\
 Commands:\n\
     test_root            test function root()\n\
     test_integral        test function integral()\n\
+    test_default         run default tests\
 \n\
     Usage: program <command> [options]\n\
 \n\
@@ -96,8 +98,57 @@ Examples:\n\
     program test_root -a -1 -b 1.01 -eps 0.1 -func1 3 -func2 1\n\
 ";
 
+const char *verbose_text =
+"\
+    In this project, it was required to implement a numerical method that allows, with a given precision eps,\n\
+    to calculate the area of a plane figure bounded by three curves, the equations\n\
+    which f1 (x) = 0.35 x ^ 2 -0.95 x + 2.7, f2 (x) = 3 x + 1, f3 (x) = 1 / (x + 2)\n\
+    the calculation of the function values had to be implemented using the NASM assembler.\n\
+    To find the area, the trapezoid method was used - a method which consists in replacing the integrand on each\n\
+    elementary segment with a polynomial of the first degree, that is, a linear function. The area under the graph of\n\
+    the function with this approach is approximated by rectangular trapezoids.\n\
+\n\
+    Let clarify that F (x) is the function of the difference f (x) - g (x) on the segment [a, b],\n\
+    that is, the zeros of the function F (x) are the intersection points of the functions f(x) and g(x).\n\
+    On the segment [a, b], the function F (x) has one isolated root. Such a segment should be chosen based on\n\
+    the analysis of the functions f(x) and g(x) and their derivatives.\n\
+    The first and second derivatives of the function F (x) on the segment [a, b] are continuous and do not equal to zero\n\
+    ,that is, they have a constant sign (> 0 or <0).\n\
+\n\
+    Newton's method, also known as the Newton–Raphson method, was used to find the intersection points of the functions.\n\
+    The main idea of the method: an initial approximation is set near the supposed root, after which a tangent to the graph\n\
+    of the function under study is constructed at the approximation point, for which the intersection with the abscissa axis is found.\n\
+     This point is taken as the next approximation. And so on, until the required accuracy is achieved.\
+";
+
+/*
+//first test example
+double g1 (double x) { return exp(2 * x - 15);}
+double g2 (double x) { return 5 * sin(x);}
+double g3 (double x) { return 2 * atan(x);}
+double dg1 (double x) { return 2 * exp(2 * x - 15);}
+double dg2 (double x) { return 5 * cos(x);}
+double dg3 (double x) { return 2 / (1 + x * x);}
+
+// second test example
+double h1 (double x) { return 10 * log(x);}
+double h2 (double x) { return x * x * x;}
+double h3 (double x) { return x * x * x * x - x - 20;}
+double dh1 (double x) { return 10 / x;}
+double dh2 (double x) { return 3 * x * x;}
+double dh3 (double x) { return 4 * x * x * x - 1;}*/
+
+void test (const char *mode, const char* func, double left, double right, double expect, double res, double eps1) {
+    printf("%s of %s in [%f; %f], expected %f, calculated %f\n", mode, func, left, right, expect, res);
+    if (fabs(res - expect) < eps1)
+        printf("GOOD! Calculating answer differs from expected less than %f\n\n", eps1);
+    else
+        printf("BAD! Calculating answer differs from expected more than %f\n\n", eps1);
+}
+
 int main (int argc, char **argv) {
     double epss = eps / 10, a = -1.99, b = 1;
+    const char *mode_root = "root", *mode_int = "integral";
     double (*foo1)(double) = f1;
     double (*foo2)(double) = f2;
     double (*dfoo1)(double) = df1;
@@ -105,6 +156,10 @@ int main (int argc, char **argv) {
     if (argc > 1) {
         if (!strcmp(argv[1], "-help")) {
             printf("%s", help_text);
+            return 0;
+        }
+        if ((!strcmp(argv[1], "-verbose")) || (!strcmp(argv[1], "-v"))) {
+            printf("%s", verbose_text);
             return 0;
         }
         for (int i = 1; i < argc; i++) {
@@ -116,12 +171,19 @@ int main (int argc, char **argv) {
                 MODE = TEST_ROOT;
             else if (!strcmp(argv[i], "test_integral"))
                 MODE = TEST_INTEGRAL;
+            else if (!strcmp(argv[i], "test_default"))
+                MODE = TEST_DEFAULT;
             else if (!strcmp(argv[i], "-a"))
                 a = strtod(argv[++i], NULL);    // next param - left bound of segment - from string to double
             else if (!strcmp(argv[i], "-b"))
                 b = strtod(argv[++i], NULL);    // next param - right bound of segment - from string to double
-            else if (!strcmp(argv[i], "-eps"))
+            else if (!strcmp(argv[i], "-eps")) {
                 epss = strtod(argv[++i], NULL);  // next param - precision - from string to double
+                if (epss <= 0) {
+                    printf("incorrect eps, program has set default eps = 0.001\n");
+                    epss = 0.001;
+                }
+            }
             else if (!strcmp(argv[i], "-func1") || !strcmp(argv[i], "-func")) // choice func for test root or integral
                 switch (atoi(argv[++i])) {
                     case 1:
@@ -135,6 +197,9 @@ int main (int argc, char **argv) {
                     case 3:
                         foo1 = f3;
                         dfoo1 = df3;
+                        break;
+                    default:
+                        printf("incorrect number of foo1, program has set previous value for func\n");
                 }
             else if (!strcmp(argv[i], "-func2"))   //choice func for test root or integral
                 switch (atoi(argv[++i])) {
@@ -149,11 +214,20 @@ int main (int argc, char **argv) {
                     case 3:
                         foo2 = f3;
                         dfoo2 = df3;
+                        break;
+                    default:
+                        printf("incorrect number of foo2, program has set previous value for func\n");
                 }
         }
     }
-
+    if (a > b) {
+        printf("incorrect input: a > b, program has set default a and b\n");
+        a = -1.99;
+        b = 1;
+    }
     if (MODE == TEST_ROOT) {
+        if (((f1 == foo1) && (f2 == foo2)) || ((f2 == foo1) && (f1 == foo2)))
+            b = -1.3;
         printf("a = %f, b = %f, eps = %f\n\n", a, b, epss);
         double x = root(foo1, foo2, a, b, epss, dfoo1, dfoo2);
         if (!PRINT_POINTS)
@@ -164,6 +238,28 @@ int main (int argc, char **argv) {
         printf("a = %f, b = %f, eps = %f\n\n", a, b, epss);
         printf("Integral = %f\n", integral(foo1, a, b, epss));
     }
+    /*else if (MODE == TEST_DEFAULT) {
+        double root1 = root(g1, g2, 6, 8.1, epss, dg1, dg2), root2 = root(g2, g3, 6, 8.1, epss, dg2, dg3),
+        root3 = root(g3, g1, 6, 8.1, epss, dg3, dg1),
+        int1 = integral(g1, root1 , root3, epss), int2 = integral(g2, root1, root2, epss),
+        int3 = integral(g3, root2, root3, epss);
+        test(mode_root, "5sinx - e^(2x-15)", 6, 8.1, 6.301380, root1, epss);
+        test(mode_root, "2arctgx - 5sinx", 6, 8.1, 6.890506, root2, epss);
+        test(mode_root, "2arctgx - e^(2x-15)", 6, 8.1, 8.031292, root3, epss);
+        test(mode_int, "e^(2x - 15)", root1, root3, 1.40144, int1, epss);
+        test(mode_int, "5sinx", root1, root2, 0.893273, int2, epss);
+        test(mode_int, "2arctgx", root2, root3, 3.27932, int3, epss);
+
+        printf("another test!\n");
+        root1 = root(h1, h2, 1, 1.5, epss, dh1, dh2),
+        root2 = root(h2, h3, 2, 3, epss, dh2, dh3),
+        root3 = root(h3, h1, 2, 3, epss, dh3, dh1),
+        test(mode_root,"10lnx - x^3", 1, 1.5, 1.177194, root1, epss);
+        test(mode_root, "x^4 - x - 20 - x^3", 2, 3, 2.477724, root2, epss);
+        test(mode_root, "x^4 - x - 20 - 10lnx", 2, 3, 2.35844, root3, epss);
+        test(mode_int,"lnx", 1, 2, 3.862943, integral(h1, 1, 2, epss), epss);
+        test(mode_int,"x^4 - x - 20", 3, 5, 528.40000, integral(h3, 3, 5, epss), epss);
+    }*/
     else { // MODE == DEFAULT
         printf("a = %f, b = %f, eps1 = eps2 = %f\n", a, b, epss);
         double root12, root13, root23;
